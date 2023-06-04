71272cb59066986d38a8e69617757251609a3134
Enes Ergen <eenes44@gmail.com>
2023-06-04 15:43:53 +0300

    Initial commit

---
+++ src/main/java/com/enesergen/itilBackend/ItilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enesergen.itilBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItilBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enesergen/itilBackend/ItilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesergen.itilBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
