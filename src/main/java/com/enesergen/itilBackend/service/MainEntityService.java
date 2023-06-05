223cf55aa82b3fdc40484d0e258a622b00e23227
Enes Ergen <eenes44@gmail.com>
2023-06-05 22:36:37 +0300

    constant was added

--- src/main/java/com/enesergen/itilBackend/service/MainEntityService.java
+++ src/main/java/com/enesergen/itilBackend/service/MainEntityService.java
@@ -1,5 +1,6 @@
 package com.enesergen.itilBackend.service;
 
+import com.enesergen.itilBackend.constant.Constant;
 import com.enesergen.itilBackend.dto.AddMainEntityRequestDto;
 import com.enesergen.itilBackend.dto.AddMainEntityResponseDto;
 import com.enesergen.itilBackend.entity.MainEntity;
@@ -11,32 +12,60 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class MainEntityService {
     private final MainEntityRepository mainEntityRepository;
-public AddMainEntityResponseDto addMainEntity(AddMainEntityRequestDto requestDto){
 
-    var entityObj= MainEntity.builder()
-            .responseTime(requestDto.getResponseTime())
-            .responseTimeSeverity(requestDto.getResponseTimeSeverity())
-            .maintenanceTime(requestDto.getMaintenanceTime())
-            .maintenanceTimeSeverity(requestDto.getMaintenanceTimeSeverity())
-            .interruptionTime(requestDto.getInterruptionTime())
-            .interruptionTimeSeverity(requestDto.getInterruptionTimeSeverity())
-            .backUpTime(requestDto.getBackUpTime())
-            .backUpTimeSeverity(requestDto.getBackUpTimeSeverity())
-            .recoveryTime(requestDto.getRecoveryTime())
-            .recoveryTimeSeverity(requestDto.getRecoveryTimeSeverity())
-            .monitoring(requestDto.getMonitoring())
-            .monitoringSeverity(requestDto.getMonitoringSeverity())
-            .networkUsage(requestDto.getNetworkUsage())
-            .networkUsageSeverity(requestDto.getNetworkUsageSeverity())
-            .memoryUsage(requestDto.getMemoryUsage())
-            .memoryUsageSeverity(requestDto.getMemoryUsageSeverity())
-            .cpuUsage(requestDto.getCpuUsage())
-            .cpuUsageSeverity(requestDto.getCpuUsageSeverity())
-            .diskUsage(requestDto.getDiskUsage())
-            .diskUsageSeverity(requestDto.getDiskUsageSeverity())
-            .build();
-            mainEntityRepository.save(entityObj);
-    System.out.println(requestDto.toString());
-    return new AddMainEntityResponseDto("Successfull");
-}
+    public MainEntity dtoToObject(AddMainEntityRequestDto requestDto) {
+        return MainEntity.builder()
+                .responseTime(requestDto.getResponseTime())
+                .responseTimeSeverity(requestDto.getResponseTimeSeverity())
+                .maintenanceTime(requestDto.getMaintenanceTime())
+                .maintenanceTimeSeverity(requestDto.getMaintenanceTimeSeverity())
+                .interruptionTime(requestDto.getInterruptionTime())
+                .interruptionTimeSeverity(requestDto.getInterruptionTimeSeverity())
+                .backUpTime(requestDto.getBackUpTime())
+                .backUpTimeSeverity(requestDto.getBackUpTimeSeverity())
+                .recoveryTime(requestDto.getRecoveryTime())
+                .recoveryTimeSeverity(requestDto.getRecoveryTimeSeverity())
+                .monitoring(requestDto.getMonitoring())
+                .monitoringSeverity(requestDto.getMonitoringSeverity())
+                .networkUsage(requestDto.getNetworkUsage())
+                .networkUsageSeverity(requestDto.getNetworkUsageSeverity())
+                .memoryUsage(requestDto.getMemoryUsage())
+                .memoryUsageSeverity(requestDto.getMemoryUsageSeverity())
+                .cpuUsage(requestDto.getCpuUsage())
+                .cpuUsageSeverity(requestDto.getCpuUsageSeverity())
+                .diskUsage(requestDto.getDiskUsage())
+                .diskUsageSeverity(requestDto.getDiskUsageSeverity())
+                .build();
+    }
+    public String responseTimeReport(int value){
+        if(value==100){
+            return Constant.RESPONSE_100;
+        } else if (value==85) {
+            return Constant.RESPONSE_85;
+        }else if(value==70){
+            return Constant.RESPONSE_70;
+        } else if (value==30) {
+            return Constant.RESPONSE_30;
+        }else{
+            throw new  Error("Invalid Value");
+        }
+    }
+    public String maintenanceTimeReport(int value){
+        if(value==100){
+            return Constant.MAINTENANCE_100;
+        } else if (value==80) {
+            return Constant.MAINTENANCE_80;
+        }else if(value==60){
+            return Constant.MAINTENANCE_60;
+        } else if (value==30) {
+            return Constant.MAINTENANCE_30;
+        }else{
+            throw new  Error("Invalid Value");
+        }
+    }
+    public AddMainEntityResponseDto addMainEntity(AddMainEntityRequestDto requestDto) {
+        var entityObj = dtoToObject(requestDto);
+        mainEntityRepository.save(entityObj);
+        return new AddMainEntityResponseDto("Successfull");
+    }
 }
